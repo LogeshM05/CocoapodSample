e7de26b318d191a574ef75a17faf6be1e6e1f1ea
LogeshM05 <logeshwarlogs05@gmail.com>
2023-10-31 15:00:56 +0530

    Added test cases

---
+++ SwiftLib/SwiftLib.swift
@@ -0,0 +1,22 @@
+//
+//  SwiftLib.swift
+//  SwiftLib
+//
+//  Created by Logeshwar M on 31/10/23.
+//
+
+import Foundation
+
+public final class SwiftyLib {
+
+    let name = "SwiftyLib"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    
+}
